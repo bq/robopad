ff05d8b6a557a44a868cdebc83298806038a2c66
Estefania Sarasola Elvira <estefania.sarasola@bq.com>
2014-07-01 16:20:21 +0200

    Added some missing files in the last commit

---
+++ RoboPad/src/main/java/com/bq/robotic/robopad/listeners/RobotListener.java
@@ -0,0 +1,61 @@
+/*
+* This file is part of the RoboPad
+*
+* Copyright (C) 2013 Mundo Reader S.L.
+* 
+* Date: February 2014
+* Author: Estefanía Sarasola Elvira <estefania.sarasola@bq.com>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 2 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+package com.bq.robotic.robopad.listeners;
+
+/**
+ * Listener for robot fragments to communicate with the fragment activity that manage the bluetooth 
+ * connection.
+ */
+
+public interface RobotListener {
+	
+	/**
+	 * Callback from the RobotFragment for checking if the device is connected to an Arduino 
+	 * through the bluetooth connection.
+	 * If the device is not connected it warns the user of it through a Toast.
+	 * 
+	 * @return true if is connected or false if not
+	 */
+	boolean onCheckIsConnected();
+	
+	
+	/**
+	 * Callback from the RobotFragment for checking if the device is connected to an Arduino 
+	 * through the bluetooth connection
+	 * without the warning toast if is not connected. 
+	 * 
+	 * @return true if is connected or false if not
+	 */
+	boolean onCheckIsConnectedWithoutToast();
+	
+	
+	/**
+	 * Callback from the RobotFragment for sending a message to the Arduino through the bluetooth 
+	 * connection. 
+	 * 
+	 * @param message to be send to the Arduino
+	 */
+	void onSendMessage(String message);
+	
+}
